c83424d6b81e53a76821fb4ecebf73acf92dd31f
Angel Barranco <angel@Angels-Mac-mini.local>
2021-10-06 14:34:28 -0700

    ContactCard.js styles with React Bootstrap and vanilla CSS done

--- src/components/contacts/ContactCard.js
+++ src/components/contacts/ContactCard.js
@@ -4,16 +4,16 @@ import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 const ContactCard = ({ firstName='', lastName='', phoneNumber='', profilePic='' }) => {
     return (
         <Container className='card-container' fluid >
-            <Row>
-                <Col xs={6} md={4}>
+            <Row className='card-row'>
+                <Col className='image-col' xs>
                     <Image src={profilePic} alt={`Profile of ${firstName} ${lastName}.`} width='96' roundedCircle fluid />
                 </Col>
-                <Col xs={6} md={4}>
+                <Col xs={8}>
                     <p>{firstName} {lastName}</p>
                     <p>{phoneNumber}</p>
                 </Col>
-                <Col xs={6} md={4}>
-                <Button variant='secondary' size='sm'>X</Button>
+                <Col className='button-col' xs>
+                <button className='contact-button'>X</button>
                 </Col>
             </Row>
             
